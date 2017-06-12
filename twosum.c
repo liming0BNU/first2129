b05b25ca767ee1bb9800903bad33cef480f7356a
liming_bnu <liming_bnu@163.com>
2017-06-12 20:53:13 +1000

    读写文件， 宏定义

--- twosum.c
+++ twosum.c
@@ -1,5 +1,7 @@
 #include <stdio.h> //如果是引号的话， 则是从当前目录找头文件，尖括号则是从标准库找
 
+#define CIRCLE_SIZE 4
+
 int gi;
 
 void usage(int argc){
@@ -10,14 +12,32 @@ void usage(int argc){
 
 int main(int argc, char const *argv[])
 {	
+	int friend_graph[CIRCLE_SIZE][CIRCLE_SIZE];
+	// int ** friend_graph;
 	if (argc!=2) // 限定只能输入一个参数， 不能多也不能少
 	{
 		usage(argc);
 		return 1;
-	} 
+	}
 	FILE* pfile;
-	pfile=fopen("mandy.txt","w+");//note: 记得open了之后， 让文件句柄接住它哟！
-	fprintf(pfile, "%s %f\n", argv[1], 3.14);//note: 字符串就是“”里面的东西
+	pfile=fopen("friend_circle.txt","r");//note: 记得open了之后， 让文件句柄接住它哟！
+	
+	//note: 如何读文件？？？ 一排一排的读文件呗
+	for (int i = 0; i < CIRCLE_SIZE; ++i)
+	{
+		for(int j=0; j<CIRCLE_SIZE; j++){
+		  	fscanf (pfile, "%d", &friend_graph[i][j]);
+		}
+	}
+
+	for (int i = 0; i < CIRCLE_SIZE; ++i)
+	{
+		for (int j = 0; j < CIRCLE_SIZE; j++)
+		{
+			printf("%d ", friend_graph[i][j]);
+		}
+		printf("\n");
+	}
 	fclose(pfile);
 	return 0;
 }
